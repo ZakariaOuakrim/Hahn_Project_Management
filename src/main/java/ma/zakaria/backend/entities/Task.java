3da9e4eeb296c68a1f1740f1ee524f26600f55a4
zakariaOuakrim <zakaria.ouakrim@marketingconfort.com>
2025-12-23 15:53:51 +0100

    Task entity

--- src/main/java/ma/zakaria/backend/entities/Task.java
+++ src/main/java/ma/zakaria/backend/entities/Task.java
@@ -1,7 +1,27 @@
 package ma.zakaria.backend.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
 public class Task {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Date createdDate;
+    private boolean completed;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+
 }
