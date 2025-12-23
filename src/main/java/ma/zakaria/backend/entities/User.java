baffa0719e8a6fb10fd1be9f7470791ee639ef79
zakariaOuakrim <zakaria.ouakrim@marketingconfort.com>
2025-12-23 15:57:13 +0100

    user entity

--- src/main/java/ma/zakaria/backend/entities/User.java
+++ src/main/java/ma/zakaria/backend/entities/User.java
@@ -1,7 +1,17 @@
 package ma.zakaria.backend.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class User {
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Project> projects;
 }
